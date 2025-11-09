44470011a77574aabdf566cddfbbb0121ec56335
misemise8 <semi.twitter42@gmail.com>
2025-11-09 21:06:39 +0900

    まだバグ

--- src/main/java/net/misemise/client/BlockHighlightRenderer.java
+++ src/main/java/net/misemise/client/BlockHighlightRenderer.java
@@ -1,28 +1,37 @@
 package net.misemise.client;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.*;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
 import net.misemise.OreMiner;
-import org.joml.Matrix4f;
-import org.lwjgl.opengl.GL11;
 
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * ブロックハイライトレンダラー
+ * WorldRenderer.drawShapeOutlineを使用してブロックのアウトラインを描画
+ */
 public class BlockHighlightRenderer {
     private static final Set<BlockPos> highlightedBlocks = new HashSet<>();
 
+    // ハイライトの色 (RGBA)
     private static final float RED = 0.0f;
     private static final float GREEN = 0.8f;
     private static final float BLUE = 1.0f;
     private static final float ALPHA = 0.5f;
 
     public static void register() {
-        OreMiner.LOGGER.info("BlockHighlightRenderer initialized (using Mixin)");
+        OreMiner.LOGGER.info("BlockHighlightRenderer initialized");
     }
 
     public static void setHighlightedBlocks(Set<BlockPos> blocks) {
@@ -46,11 +55,13 @@ public class BlockHighlightRenderer {
         }
     }
 
-    public static void render(Camera camera) {
+    public static void render(Camera camera, VertexConsumerProvider.Immediate vertexConsumers) {
+        Set<BlockPos> blocksCopy;
         synchronized (highlightedBlocks) {
             if (highlightedBlocks.isEmpty()) {
                 return;
             }
+            blocksCopy = new HashSet<>(highlightedBlocks);
         }
 
         MinecraftClient client = MinecraftClient.getInstance();
@@ -62,79 +73,38 @@ public class BlockHighlightRenderer {
             MatrixStack matrices = new MatrixStack();
             Vec3d cameraPos = camera.getPos();
 
+            matrices.push();
             matrices.translate(-cameraPos.x, -cameraPos.y, -cameraPos.z);
 
-            // OpenGL状態を設定
-            RenderSystem.enableBlend();
-            RenderSystem.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-            RenderSystem.disableDepthTest();
-            RenderSystem.disableCull();
-            GL11.glLineWidth(2.0f);
+            // RenderLayer.getLinesを使用してラインを描画
+            VertexConsumer vertexConsumer = vertexConsumers.getBuffer(RenderLayer.getLines());
 
-            // シェーダーを設定
-            RenderSystem.setShader(GameRenderer::getPositionColorProgram);
+            for (BlockPos pos : blocksCopy) {
+                Box box = new Box(pos).expand(0.002);
 
-            Tessellator tessellator = Tessellator.getInstance();
-            BufferBuilder buffer = tessellator.begin(
-                    VertexFormat.DrawMode.DEBUG_LINES,
-                    VertexFormats.POSITION_COLOR
-            );
+                // Box -> VoxelShape に変換（shapes() は存在しないのでこっちを使う）
+                VoxelShape shape = VoxelShapes.cuboid(
+                        box.minX, box.minY, box.minZ,
+                        box.maxX, box.maxY, box.maxZ
+                );
 
-            Matrix4f posMatrix = matrices.peek().getPositionMatrix();
-
-            synchronized (highlightedBlocks) {
-                for (BlockPos pos : highlightedBlocks) {
-                    drawBox(buffer, posMatrix, pos);
-                }
+                // WorldRendererのdrawShapeOutlineを使用
+                WorldRenderer.drawShapeOutline(
+                        matrices,
+                        vertexConsumer,
+                        shape,
+                        0.0, 0.0, 0.0, // 既に行列でカメラ分を引いているので 0,0,0 で OK
+                        RED, GREEN, BLUE, ALPHA
+                );
             }
 
-            BufferedRenderer.drawWithGlobalProgram(buffer.end());
+            matrices.pop();
 
-            // 元に戻す
-            RenderSystem.enableDepthTest();
-            RenderSystem.disableBlend();
-            RenderSystem.enableCull();
-            GL11.glLineWidth(1.0f);
+            // バッファをフラッシュ
+            vertexConsumers.draw();
 
         } catch (Exception e) {
             OreMiner.LOGGER.error("Error rendering highlights", e);
         }
-    }
-
-    private static void drawBox(BufferBuilder buffer, Matrix4f matrix, BlockPos pos) {
-        float x = pos.getX();
-        float y = pos.getY();
-        float z = pos.getZ();
-
-        float x1 = x - 0.002f;
-        float y1 = y - 0.002f;
-        float z1 = z - 0.002f;
-        float x2 = x + 1.002f;
-        float y2 = y + 1.002f;
-        float z2 = z + 1.002f;
-
-        // 底面
-        line(buffer, matrix, x1, y1, z1, x2, y1, z1);
-        line(buffer, matrix, x2, y1, z1, x2, y1, z2);
-        line(buffer, matrix, x2, y1, z2, x1, y1, z2);
-        line(buffer, matrix, x1, y1, z2, x1, y1, z1);
-
-        // 上面
-        line(buffer, matrix, x1, y2, z1, x2, y2, z1);
-        line(buffer, matrix, x2, y2, z1, x2, y2, z2);
-        line(buffer, matrix, x2, y2, z2, x1, y2, z2);
-        line(buffer, matrix, x1, y2, z2, x1, y2, z1);
-
-        // 縦
-        line(buffer, matrix, x1, y1, z1, x1, y2, z1);
-        line(buffer, matrix, x2, y1, z1, x2, y2, z1);
-        line(buffer, matrix, x2, y1, z2, x2, y2, z2);
-        line(buffer, matrix, x1, y1, z2, x1, y2, z2);
-    }
-
-    private static void line(BufferBuilder buffer, Matrix4f matrix,
-                             float x1, float y1, float z1, float x2, float y2, float z2) {
-        buffer.vertex(matrix, x1, y1, z1).color(RED, GREEN, BLUE, ALPHA);
-        buffer.vertex(matrix, x2, y2, z2).color(RED, GREEN, BLUE, ALPHA);
     }
 }
--- src/main/java/net/misemise/mixin/WorldRendererMixin.java
+++ src/main/java/net/misemise/mixin/WorldRendererMixin.java
@@ -1,24 +1,28 @@
 package net.misemise.mixin;
 
-import com.mojang.blaze3d.buffers.GpuBufferSlice;
 import net.minecraft.client.render.*;
-import net.minecraft.client.util.ObjectAllocator;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.math.BlockPos;
 import net.misemise.client.BlockHighlightRenderer;
-import org.joml.Matrix4f;
-import org.joml.Vector4f;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WorldRenderer.class)
-public class WorldRendererMixin {
+public abstract class WorldRendererMixin {
 
-    @Inject(method = "render", at = @At("RETURN"))
-    private void onRender(ObjectAllocator allocator, RenderTickCounter tickCounter, boolean renderBlockOutline, Camera camera, Matrix4f positionMatrix, Matrix4f matrix4f, Matrix4f projectionMatrix, GpuBufferSlice fogBuffer, Vector4f fogColor, boolean renderSky, CallbackInfo ci) {
-        // レンダリング後にハイライトを描画
-        BlockHighlightRenderer.render(camera);
+    @Shadow
+    private VertexConsumerProvider.Immediate vertexConsumerProvider;
+
+    @Inject(method = "render", at = @At("TAIL"))
+    private void onRenderEnd(RenderTickCounter tickCounter, boolean renderBlockOutline,
+                             Camera camera, GameRenderer gameRenderer,
+                             LightmapTextureManager lightmapTextureManager,
+                             MatrixStack matrices, MatrixStack matrices2, CallbackInfo ci) {
+        // レンダリングの最後にハイライトを描画
+        if (vertexConsumerProvider != null) {
+            BlockHighlightRenderer.render(camera, vertexConsumerProvider);
+        }
     }
 }
